39d9b3603cef53910ad008a515da5f705b437a14
Gavin Ching <gavinchingy@gmail.com>
2014-04-06 18:35:36 -0400

    Finalize sort and start destruction

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -3,10 +3,13 @@
 // Gavin Ching
 // March 30, 2014
 
-
 #include "benchmarks.hpp"
 
 int main(){
-	benching_structures(1000000);
+	// Tests # of n data from the interval of 100000 to 1000000
+	for (int n = 100000; n <= 1000000; n=n+100000){
+	  benching_structures(n);
+	}
+	
 	return 0;
 }
--- benchmarks.hpp
+++ benchmarks.hpp
@@ -29,59 +29,86 @@ struct triv {
 // Main benching function for inserting
 void benching_insert(int numberOfInput);
 
-// Benching for deque insert.
+// Benching for insert from top.
 microseconds insert_deque(int numberOfInput);
-
-// Benching for list insert.
 microseconds insert_list(int numberOfInput);
-
-// Benching for stack insert.
 microseconds insert_stack(int numberOfInput);
 
+// Benching for sort.
+microseconds sort_deque(int numberOfInput);
+microseconds sort_list(int numberOfInput);
 
+// Benching for destruction.
+microseconds destruction_deque(int numberOfInput);
+microseconds destruction_list(int numberOfInput);
+microseconds destruction_stack(int numberOfInput);
 
+// Benching for getting top elements and delete.
+microseconds top_deque(int numberOfInput);
+microseconds top_list(int numberOfInput);
+microseconds top_stack(int numberOfInput);
 
 
 /************************************************************************/
 
 
-/*************************Function Descriptions ******************************/
+/************************* Main Functions ******************************/
 
 // benching_insert
 // int numberOfInput - The number of data that will be inserted into the data structures.
 // Instantiates tests for inserts of the data strucutres.
-// List will have data pushed from the back.
+// List will have data pushed from the front
 // Stack will have data inserted by push.
-// Deqye will have data be inserted by pushing from the back.
+// Deqye will have data be inserted by pushing from the front.
 void benching_structures(int numberOfInput){
 
+  // Output the number N to the console.
+  std::cout << "The number of inputs, n, for this benchmark is " << numberOfInput << std::endl;
+
   // Initialize the variables to hold the total time of inserts for data structures.
-  microseconds results_deque (0);
-  microseconds results_list (0);
-  microseconds results_stack (0);
+  microseconds results_insert_deque (0);
+  microseconds results_insert_list (0);
+  microseconds results_insert_stack (0);
+  microseconds results_sort_deque (0);
+  microseconds results_sort_list (0);
+
 
   // Iterate through the number of repetitions and call the insert functions at each.
   for (std::size_t i = 0; i < REP; i++){
   	// Calculate the time it takes for each data strucutre inserts.
   	// Add up time for each data strucutre specifically.
   	
-  	// Queue inserts.
-  	results_deque += insert_deque(numberOfInput);
+  	// Deque inserts.
+  	results_insert_deque += insert_deque(numberOfInput);
 
   	// List inserts.
-  	results_list += insert_list(numberOfInput);
+  	results_insert_list += insert_list(numberOfInput);
 
   	// Stack inserts.
-  	results_stack += insert_stack(numberOfInput);
+  	results_insert_stack += insert_stack(numberOfInput);
+
+    // Deque sort.
+    results_sort_deque += sort_deque(numberOfInput);
+
+    // List sort.
+    results_sort_list += sort_list(numberOfInput);
   }
   // Output the average of the inserts depending on the number of repetitions set.
-  std::cout << "Deque insert: " << results_deque.count()/REP << std::endl;
+  std::cout << "Deque insert from front: " << results_insert_deque.count()/REP << std::endl;
 
-  std::cout << "List insert from back: " << results_list.count()/REP << std::endl;
+  std::cout << "List insert from front: " << results_insert_list.count()/REP << std::endl;
 
-  std::cout << "Stack inserts: " << results_stack.count()/REP << std::endl;
+  std::cout << "Stack inserts: " << results_insert_stack.count()/REP << std::endl;
 
+  std::cout << "Deque sort: " << results_sort_deque.count()/REP << std::endl;
+  
+  std::cout << "List sort: " << results_sort_list.count()/REP << std::endl;
 }
+
+/************************************************************************/
+
+
+/****************************** Insert ********************************/
 
 // insert_deque
 // int numberOfInput - The number of data that will be inserted into the deque.
@@ -92,13 +119,13 @@ microseconds insert_deque(int numberOfInput){
   // Initialize the deque.
   std::deque<triv> trivial_deque;
 
-  // Push j into back of deque up to the given number of input and time it.
+  // Push j into front of deque up to the given number of input and time it.
   // Data being pushed will be of the strucutre triv with size_t as an attribute..
   auto start = std::chrono::system_clock::now();
   for (std::size_t j = 0; j < numberOfInput; j++){
   	triv triv_data;
   	triv_data.x = j;	
-    trivial_deque.push_back(triv_data);
+    trivial_deque.push_front(triv_data);
   }
   auto end = std::chrono::system_clock::now();
   microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
@@ -115,12 +142,12 @@ microseconds insert_list(int numberOfInput){
   std::list<triv> trivial_list;
 
   // Push j into list up to the given number of input and time it.
-  // Data being pushed to the back of the list will be of triv with size_t as an attribute.
+  // Data being pushed to the front of the list will be of triv with size_t as an attribute.
   auto start = std::chrono::system_clock::now();
   for (std::size_t j = 0; j < numberOfInput; j++){
   	triv triv_data;
   	triv_data.x = j;	
-    trivial_list.push_back(triv_data);
+    trivial_list.push_front(triv_data);
   }
   auto end = std::chrono::system_clock::now();
   microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
@@ -133,7 +160,7 @@ microseconds insert_list(int numberOfInput){
 // Insert function for stack. Data will be pushed onto it.
 // Returns the time elapsed in microseconds.
 microseconds insert_stack(int numberOfInput){
-  
+
   // Initialize the stack.
   std::stack<triv> trivial_stack;
 
@@ -151,5 +178,149 @@ microseconds insert_stack(int numberOfInput){
 
 
 }
+
+/************************************************************************/
+
+
+/****************************** Sort **********************************/
+// sort_deque
+// int numberOfInput - Number of data in the deque.
+// Sorts the given data in the deque and return time elapsed in microseconds.
+// Fake random data will be pushed from the back.
+microseconds sort_deque(int numberOfInput){
+  //Instatiate trivial deque with random data.
+  std::deque<triv> triv_deque;
+  for (std::size_t j = 0; j < numberOfInput; j++){
+    triv triv_data;
+    triv_data.x = rand()%numberOfInput;    
+    triv_deque.push_back(triv_data);
+  }
+
+  // Sort the data using std::sort and time it
+  auto start = std::chrono::system_clock::now();
+  std::sort(std::begin(triv_deque), std::end(triv_deque));
+  auto end = std::chrono::system_clock::now();
+
+  microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
+
+  return elapsed;
+
+}
+
+// sort_list
+// int numberOfInput - Number of data in the list.
+// Sorts the given data in the list and returns time in microseconds.
+// Fake random data will be pushed from the back of the list.
+microseconds sort_list(int numberOfInput){
+  //Instantiate trivial list with random data.
+  std::list<triv> triv_list;
+  for(std::size_t j =0; j < numberOfInput; j++){
+    triv triv_data;
+    triv_data.x = rand()%numberOfInput;
+    triv_list.push_back(triv_data);
+  }
+
+  // Sort using the given containers sort function and time it.
+  auto start = std::chrono::system_clock::now();
+  triv_list.sort();
+  auto end = std::chrono::system_clock::now();
+
+  microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
+
+  return elapsed;
+
+}
+
+
+
+/************************************************************************/
+
+/******************************** Destruction ********************************/
+// destruction_deque
+// int numberOfInput - number of data that needs to be deleted
+// Calculates time it takes to fully delete the deque given the number of input.
+// Returns the time in microseconds.
+microseconds destruction_deque(int numberOfInput){
+  // Instantiate deque with random data.
+  std::deque<triv> triv_deque;
+  for (std::size_t j = 0; j < numberOfInput; j++){
+    triv triv_data;
+    triv_data.x = rand()%numberOfInput;    
+    triv_deque.push_back(triv_data);
+  }
+
+  //Record time of clearing the deque using the given container function.
+  auto start = std::chrono::system_clock::now();
+  triv_deque.clear();
+  auto end = std::chrono::system_clock::now();
+
+  microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
+
+  return elapsed;
+
+
+}
+
+// destruction_list
+// int numberOfInput - number of data that needs to be deleted
+// Calculates time it takes to fully delete the list given the number of input.
+// Returns the time in microseconds.
+microseconds destruction_list(int numberOfInput){
+
+  //Instantiate trivial list with random data.
+  std::list<triv> triv_list;
+  for(std::size_t j =0; j < numberOfInput; j++){
+    triv triv_data;
+    triv_data.x = rand()%numberOfInput;
+    triv_list.push_back(triv_data);
+  }
+
+  // Delete using the given containers sort function and time it.
+  auto start = std::chrono::system_clock::now();
+  triv_list.clear();
+  auto end = std::chrono::system_clock::now();
+
+  microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
+
+  return elapsed;
+
+
+
+}
+
+// destruction_stack
+// int numberOfInput - number of data that needs to be deleted
+// Calculates time it takes to fully delete the stack given the number of input.
+// Returns the time in microseconds.
+microseconds destruction_stack(int numberOfInput){
+
+  // Instantiate stack with random data.
+  std::stack<triv> triv_stack;
+  for (std::size_t j = 0; j < numberOfInput; j++){
+    triv triv_data;
+    triv_data.x = rand()%numberOfInput;    
+    triv_stack.push(triv_data);
+  }
+
+  // Pop till empty and time it.
+  auto start = std::chrono::system_clock::now();
+  while ( ! triv_stack.empty() ){
+      triv_stack.pop();
+  }
+  auto end = std::chrono::system_clock::now();
+
+  microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
+
+  return elapsed;
+}
+
+
+
+/************************************************************************/
+
+/******************************** Top k elements********************************/
+microseconds top_deque(int numberOfInput);
+microseconds top_list(int numberOfInput);
+microseconds top_stack(int numberOfInput);
 
 /************************************************************************/
