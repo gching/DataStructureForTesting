0609a514461e5b646088cbfc8eed04515901459b
Gavin Ching <gavinchingy@gmail.com>
2014-04-07 11:27:40 -0400

    Finalize code

--- benchmarks.hpp
+++ benchmarks.hpp
@@ -403,23 +403,12 @@ microseconds top_list(int numberOfInput, int k){
     triv_list.push_back(triv_data);
   }
 
-  // Tranverse top k elements from list and record time.
-  // Set up iterator for list so it will only read up to k elements.
-  //std::list<triv>::iterator upto = triv_list.begin();
-  // Increment the iterator to stop at the k+1 element;
-  //for (int p = 0; p < k; p++ ){
-   // upto = ++upto;
- // }
-
-  // Traverse!
+  // Traverse and pop!
   auto start = std::chrono::system_clock::now();
   for (int z = 0; z < k; z++){
     triv_list.pop_front();
   }
- // for (std::list<int>::iterator it=triv_list.begin(); it != upto; ++it){
-    // Do nothing here as we do not need to grab the data, but currently, the element is stored
-    // through the iterator - *it.
-  //}
+
   auto end = std::chrono::system_clock::now();
 
   microseconds elapsed = std::chrono::duration_cast<microseconds>(end - start);
---
+++ extra.cpp
@@ -0,0 +1,10 @@
+ // for (std::list<int>::iterator it=triv_list.begin(); it != upto; ++it){
+    // Do nothing here as we do not need to grab the data, but currently, the element is stored
+    // through the iterator - *it.
+  //}  // Tranverse top k elements from list and record time.
+  // Set up iterator for list so it will only read up to k elements.
+  //std::list<triv>::iterator upto = triv_list.begin();
+  // Increment the iterator to stop at the k+1 element;
+  //for (int p = 0; p < k; p++ ){
+   // upto = ++upto;
+ // }
